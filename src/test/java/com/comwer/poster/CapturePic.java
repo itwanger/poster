df5e7c9d4dc3c73bfd1d2f6b47af85e3340d103e
qinggee <www.qing_gee@163.com>
2019-04-28 11:32:53 +0800

    优化

--- src/test/java/com/comwer/poster/CapturePic.java
+++ src/test/java/com/comwer/poster/CapturePic.java
@@ -4,9 +4,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
-import org.apache.http.Consts;
-import org.apache.http.Header;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
@@ -14,7 +14,6 @@ import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
@@ -23,7 +22,7 @@ import org.slf4j.LoggerFactory;
 
 public class CapturePic {
 	private static Logger logger = LoggerFactory.getLogger(CapturePic.class);
-	
+
 	/**
 	 * 从 HttpResponse 实例中获取状态码、错误信息、以及响应信息等等.
 	 * 
@@ -36,7 +35,7 @@ public class CapturePic {
 		final StatusLine statusLine = response.getStatusLine();
 		// 获取响应实体
 		final HttpEntity entity = response.getEntity();
-		
+
 		// 状态码一旦大于 300 表示请求失败
 		if (statusLine.getStatusCode() >= 300) {
 			EntityUtils.consume(entity);
@@ -44,7 +43,7 @@ public class CapturePic {
 		}
 		return entity;
 	}
-	
+
 	/**
 	 * 将输入流信息读入到临时文件中
 	 * 
@@ -55,33 +54,40 @@ public class CapturePic {
 	 * @return
 	 * @throws IOException
 	 */
-	private static File createTmpFile(InputStream inputStream, String prefix, String suffix, File directory) throws IOException {
-		// 在指定目录中创建一个新的空文件，使用给定的前缀和后缀字符串生成其名称。 
+	private static File createTmpFile(InputStream inputStream, String prefix, String suffix, File directory)
+			throws IOException {
+		// 在指定目录中创建一个新的空文件，使用给定的前缀和后缀字符串生成其名称。
 		File tmpFile = File.createTempFile(prefix, suffix, directory);
-		// 文件已存在的话，删除。
-		tmpFile.deleteOnExit();
+		// 请求在虚拟机终止时删除此抽象路径名所表示的文件或目录。
+		// tmpFile.deleteOnExit();
 
 		// 新建文件输出流对象
-		try (FileOutputStream fos = new FileOutputStream(tmpFile)) {
-			int read = 0;
-			byte[] bytes = new byte[1024 * 100];
-			while ((read = inputStream.read(bytes)) != -1) {
-				fos.write(bytes, 0, read);
-			}
+		FileOutputStream fos = new FileOutputStream(tmpFile);
+		int read = 0;
 
-			fos.flush();
-			return tmpFile;
+		// 1024Byte(字节)=1KB 1024KB=1MB
+		byte[] bytes = new byte[1024 * 100];
+		while ((read = inputStream.read(bytes)) != -1) {
+			fos.write(bytes, 0, read);
 		}
+
+		fos.flush();
+		fos.close();
+		return tmpFile;
 	}
 
 	public static void main(String[] args) throws IOException {
 		logger.debug("根据输入的路径采集网络图片");
-		
+
+		// 金山词霸的图片路径
+		String formatDate = DateFormatUtils.format(new Date(), "yyyyMMdd");
+		String picURL = "http://cdn.iciba.com/news/word/big_" + formatDate + "b.jpg";
+
 		// 根据路径发起 HTTP get 请求
-		HttpGet httpget = new HttpGet(args[0]);
+		HttpGet httpget = new HttpGet(picURL);
 		// 使用 addHeader 方法添加请求头部
 		httpget.addHeader("Content-Type", "text/html;charset=UTF-8");
-		
+
 		// 配置请求的超时设置
 		RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(500).setConnectTimeout(500)
 				.setSocketTimeout(500).build();
@@ -92,14 +98,14 @@ public class CapturePic {
 		// 使用 HttpClientBuilder 创建 CloseableHttpClient 对象
 		// CloseableHttpClient 是一个抽象类，它是 HttpClient 的基本实现，也实现了java.io.Closeable。
 		CloseableHttpClient httpclient = HttpClientBuilder.create().build();
-		
+
 		// 通过执行此 HttpGet 请求获取 CloseableHttpResponse 实例
 		// 获取响应信息的输入流
 		try (CloseableHttpResponse response = httpclient.execute(httpget);
 				// InputStream 实现了 Closeable 接口
 				InputStream picStream = handleResponse(response).getContent();) {
 
-			pic = createTmpFile(picStream, "pic_" + IdGen.uuid(), ".jpg");
+			pic = createTmpFile(picStream, "pic_" + IdGen.uuid(), ".jpg", new File("D:\\test"));
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		} finally {
@@ -110,6 +116,8 @@ public class CapturePic {
 		if (pic != null && !pic.exists()) {
 			throw new IllegalArgumentException("请提供正确的网络图片路径！");
 		}
+
+		logger.debug(pic.getAbsolutePath());
 	}
 
 }
